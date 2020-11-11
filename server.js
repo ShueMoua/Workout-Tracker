f593b754c89f0a6c03f9e427bfb5b58af7b60c2a
Shuelong Moua <moua_shuelong@yahoo.com>
2020-11-11 07:26:56 -0600

    added dependencies to sever.js file

---
+++ server.js
@@ -0,0 +1,33 @@
+//Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// Require Models
+// const User = require("./userModel.js");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+
+
+
+
+
+
+
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
